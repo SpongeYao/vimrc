5d40052786e1dad3419a0c0a1a87e0b4e66aa579
spongeyao <spongeyao@gmail.com>
2017-10-24 07:37:48 +0000

    rpi version

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 sudo apt-get install vim 
 sudo apt-get install git curl
-git clone https://github.com/gmarik/vundle.git ~/.vim/bundle/vundle
-cp vimrc/vimrc ~/.vimrc
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+cp vimrc ~/.vimrc
 vim +PluginInstall +qall
